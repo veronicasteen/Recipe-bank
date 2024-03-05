7e83bf185685019912f5a844fa0011963f1e8961
veronicasteen <veronica.steen@outlook.com>
2024-03-05 19:18:12 +0100

    Basic structure

--- main.js
+++ main.js
@@ -1,10 +1,30 @@
-Vue.createApp({
-    methods: {
-    },
-    computed: {
-    },
+let app = Vue.createApp({
     data() {
         return {
+            recipes: [],
+            showRecipes: false,
+            
         };
+    },
+
+    methods: {
+        async getStarters() {
+            await this.fetchRecipes('starter');
+        },
+        async getMainCourses() {
+            await this.fetchRecipes('main course');
+        },
+        async getDesserts() {
+            await this.fetchRecipes('dessert');
+        },
+        async fetchRecipes(category) {
+
+            const response = await fetch('recipes.json'); // Adjust the path based on your file structure
+            const data = await response.json();
+            this.recipes = data.recipes.filter(recipe => recipe.mealType === category);
+            this.showRecipes = true;
+        }
     }
-}).mount('#app');
+});
+
+app.mount('#app');
