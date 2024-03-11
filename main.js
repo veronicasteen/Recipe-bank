13f61e0f711952c284e542043487a9ab229e1a9d
veronicasteen <veronica.steen@outlook.com>
2024-03-11 18:23:05 +0100

    Added copy list-button and function

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ let app = Vue.createApp({
         return {
             recipes: [],
             showRecipes: false,
-            showShoppingList: false, // Lägg till en flagga för att visa/ gömma inköpslistan
+            showShoppingList: false,
             shoppingBag: [],
             isAllergenInfoActive: false,
         };
@@ -46,26 +46,32 @@ let app = Vue.createApp({
         //metod för att lägga till titel osv i shoppinglistan
         addToShoppingBag({ title, ingredients }) {
 
-            const newItem = {
+            let newItem = {
                 title: title,
                 ingredients: ingredients,
                 checked: false,
-                quantity: 1,
-            
             };
             this.shoppingBag.push(newItem);
-       
+
         },
+
+        toggleAllergenInfo() {
+            this.isAllergenInfoActive = !this.isAllergenInfoActive;
+        },
+
+        //metod för att kolla om receptet redan finns i shoppingbagen
+        //för att inaktivera "add" knappen v
 
         isRecipeInShoppingBag(recipe) {
             return this.shoppingBag.some(item => item.title === recipe.title);
         },
+
         //metod för att checka i eller ur checkboxen i shoppinglistan
         toggleCheckbox(item) {
 
             item.checked = !item.checked;
         },
-        
+
         //metod för att ta bort icheckade items
         removeCheckedItems() {
             this.shoppingBag = this.shoppingBag.filter(item => !item.checked);
@@ -77,11 +83,21 @@ let app = Vue.createApp({
             this.shoppingBag.forEach(item => (item.checked = !allChecked));
         },
 
-        //metod för att antingen visa eller avaktivera allergi-information
-        toggleAllergenInfo() {
-            this.isAllergenInfoActive = !this.isAllergenInfoActive;
-          },
-    }
+        copyShoppingList() {
+            // Hämtar texten från inköpslistan
+            let shoppingListText = this.shoppingBag
+              .map(item => `${item.title}: ${item.ingredients.join(', ')}`)
+              .join('\n');
+          
+            // Kopierar texten 
+            navigator.clipboard.writeText(shoppingListText)
+              .then(() => {
+                // Meddela användaren att kopieringen är klar
+                alert('Your shopping-list has been copied.');
+              })
+          
+    },
+}
 });
 
 app.mount('#app');
